b87b91327d1c4182b4ad5506a21403b2efe6d417
jdabassett <jacobbassett@gmail.com>
2025-12-21 21:44:23 -0700

    fix: updated settings in digest job endpoint

--- app/helpers/digest_route.py
+++ app/helpers/digest_route.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException, status
 from sqlalchemy import desc, func, select
 from sqlalchemy.orm import Session
 
-from app.core.config import Settings
+from app.core.config import settings
 from app.models import Digest
 
 
@@ -24,11 +24,11 @@ def request_within_digest_limit_or_exception(user_id: str, session: Session) -> 
         select(func.count(Digest.id)).where(Digest.user_id == user_id)
     )
 
-    if digest_count is None or digest_count >= Settings.DIGEST_JOB_LIMIT:
+    if digest_count is None or digest_count >= settings.DIGEST_JOB_LIMIT:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=(
-                f"User has more than {Settings.DIGEST_JOB_LIMIT} digest jobs. "
+                f"User has more than {settings.DIGEST_JOB_LIMIT} digest jobs. "
                 "To keep this a free service we limit the number of user records."
                 "Please delete old records before submitting a new digest job."
             ),
@@ -62,14 +62,14 @@ def request_outside_digest_interval_or_exception(
         return
 
     time_threshold = datetime.datetime.now(datetime.UTC) - datetime.timedelta(
-        minutes=Settings.DIGEST_JOB_INTERVAL
+        minutes=settings.DIGEST_JOB_INTERVAL
     )
 
     if latest_digest.created_at >= time_threshold:
         raise HTTPException(
             status_code=status.HTTP_429_TOO_MANY_REQUESTS,
             detail=(
-                f"A digest job was submitted less than {Settings.DIGEST_JOB_INTERVAL} minutes ago. "
+                f"A digest job was submitted less than {settings.DIGEST_JOB_INTERVAL} minutes ago. "
                 "To keep this service free we meter the number of jobs per user."
                 "Please wait before submitting a new digest job."
             ),
