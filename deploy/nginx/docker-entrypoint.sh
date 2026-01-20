994933272671f745b114769d7780c4fb0dacd4a4
Jacob Bassett <jacobbassett@gmail.com>
2026-01-19 17:04:34 -0700

    Fix deployment again (#50)
    * fix: deployment
    
    * fix: address EC2 username
    
    * fix: removed quotes
    
    * fix: address missing bracket
    
    * fix: address error again
    
    * fix: another fix
    
    * fix: removed stripped part

--- deploy/nginx/docker-entrypoint.sh
+++ deploy/nginx/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# Replace placeholder in nginx config with actual API key
-sed -i "s|API_KEY_PLACEHOLDER|${API_KEY:-dev-api-key-change-me}|g" /etc/nginx/nginx.conf
-exec nginx -g 'daemon off;'
+# Copy nginx config to writable location and replace placeholder with actual API key
+cp /etc/nginx/nginx.conf /tmp/nginx.conf
+sed -i "s|API_KEY_PLACEHOLDER|${API_KEY:-dev-api-key-change-me}|g" /tmp/nginx.conf
+exec nginx -g 'daemon off;' -c /tmp/nginx.conf
